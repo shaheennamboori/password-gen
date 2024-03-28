908f2b5ddf415e2e99f4657a89aff1c6dbf4dcd9
shaheennamboori <chichushaheen@gmail.com>
2024-03-28 08:14:02 +0530

    Added publishing mechanism

--- src/App.jsx
+++ src/App.jsx
@@ -34,59 +34,63 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
-        <h1 className="text-white text-center my-3">Password Generator</h1>
-        <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input
-            type="text"
-            value={pass}
-            className="outline-none w-full py-1 px-3"
-            placeholder="password"
-            readOnly
-            ref={passwordRef}
-          />
-          <button
-            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
-            onClick={copyTo}
-          >
-            Copy
-          </button>
-        </div>
-        <div className="flex text-sm gap-x-2">
-          <div className="flex items-center gap-x-1">
+      <div className="h-lvh flex items-center">
+        <div className="flex flex-col justify-between w-full max-w-md mx-auto shadow-md rounded-lg px-4 pb-10 pt-3 my-8 text-orange-500 bg-gray-700">
+          <h1 className="text-white text-center mb-3 mt-1">
+            Password Generator
+          </h1>
+          <div className="flex shadow rounded-lg overflow-hidden mb-4">
             <input
-              type="range"
-              min={6}
-              max={100}
-              value={length}
-              className="cursor-pointer"
-              onChange={(e) => {
-                setLength(e.target.value);
-              }}
+              type="text"
+              value={pass}
+              className="outline-none w-full py-1 px-3"
+              placeholder="password"
+              readOnly
+              ref={passwordRef}
             />
-            <label> Length: {length}</label>
+            <button
+              className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+              onClick={copyTo}
+            >
+              Copy
+            </button>
           </div>
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={numAllowed}
-              id="numberInput"
-              onChange={() => {
-                setNumAllowed(!numAllowed);
-              }}
-            />
-            <label>Numbers</label>
-          </div>
-          <div className="flex items-center gap-x-1">
-            <input
-              type="checkbox"
-              defaultChecked={charAllowed}
-              id="charInput"
-              onChange={() => {
-                setCharAllowed(!charAllowed);
-              }}
-            />
-            <label>Characters</label>
+          <div className="flex text-sm gap-x-2">
+            <div className="flex items-center gap-x-1">
+              <input
+                type="range"
+                min={6}
+                max={100}
+                value={length}
+                className="cursor-pointer"
+                onChange={(e) => {
+                  setLength(e.target.value);
+                }}
+              />
+              <label> Length: {length}</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={numAllowed}
+                id="numberInput"
+                onChange={() => {
+                  setNumAllowed(!numAllowed);
+                }}
+              />
+              <label>Numbers</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={charAllowed}
+                id="charInput"
+                onChange={() => {
+                  setCharAllowed(!charAllowed);
+                }}
+              />
+              <label>Characters</label>
+            </div>
           </div>
         </div>
       </div>
